15697db704c5409da7f693d6ea3e75d701d1d0d4
AminSafari207 <safarin207368@gmail.com>
2025-08-24 09:08:19 +0330

    Add Car and Truck repository interface

---
+++ src/main/java/com/app/repository/CarRepository.java
@@ -0,0 +1,6 @@
+package com.app.repository;
+
+import com.app.model.enitty.Car;
+
+public interface CarRepository extends VehicleRepository<Long, Car> {
+}
---
+++ src/main/java/com/app/repository/TruckRepository.java
@@ -0,0 +1,6 @@
+package com.app.repository;
+
+import com.app.model.enitty.Truck;
+
+public interface TruckRepository extends VehicleRepository<Long, Truck> {
+}
