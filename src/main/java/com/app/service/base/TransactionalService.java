4aef100b3ae2d168c740b00fcca2d5ddab66ea9d
AminSafari207 <safarin207368@gmail.com>
2025-08-24 13:44:22 +0330

    Implement TransactionalService abstract class

---
+++ src/main/java/com/app/service/base/TransactionalService.java
@@ -0,0 +1,49 @@
+package com.app.service.base;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public abstract class TransactionalService {
+    private final EntityManagerFactory emf;
+
+    public TransactionalService(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    public void executeTransaction(Consumer<EntityManager> consumer) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            consumer.accept(em);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) tx.rollback();
+            throw new RuntimeException("Transaction failed.", e);
+        } finally {
+            em.close();
+        }
+    }
+
+    public <R> R executeTransaction(Function<EntityManager, R> function) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            R res = function.apply(em);
+            tx.commit();
+            return res;
+        } catch (Exception e) {
+            if (tx.isActive()) tx.rollback();
+            throw new RuntimeException("Transaction failed.", e);
+        } finally {
+            em.close();
+        }
+    }
+}
