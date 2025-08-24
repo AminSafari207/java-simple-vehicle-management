4b19a7513e079853648b78b2eda223a4a564ede1
AminSafari207 <safarin207368@gmail.com>
2025-08-24 10:56:42 +0330

    Implement VehicleService interface

--- src/main/java/com/app/service/VehicleService.java
+++ src/main/java/com/app/service/VehicleService.java
@@ -1,4 +1,19 @@
 package com.app.service;
 
-public interface VehicleService {
+import com.app.model.base.Vehicle;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public interface VehicleService<ID, T extends Vehicle<T>> {
+    T create(T vehicle);
+
+    List<T> findAll();
+    List<T> findByBrand(String brand);
+    Optional<T> findById(ID id);
+
+    T update(ID id, Map<String, Object> updateMap);
+
+    boolean deleteById(ID id);
 }
