64cea998d71b9c590dee83478022d4d3964dc04e
AminSafari207 <safarin207368@gmail.com>
2025-08-25 06:29:54 +0330

    Add VehicleNotFoundException

---
+++ src/main/java/com/app/exception/VehicleNotFoundException.java
@@ -0,0 +1,7 @@
+package com.app.exception;
+
+public class VehicleNotFoundException extends RuntimeException {
+    public VehicleNotFoundException(String message) {
+        super(message);
+    }
+}
