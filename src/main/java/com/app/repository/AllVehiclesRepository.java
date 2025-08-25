e886896fe1b44348e6e0ceb0f95298ba5a87239f
AminSafari207 <safarin207368@gmail.com>
2025-08-25 19:43:12 +0330

    Implement AllVehiclesRepository interface

---
+++ src/main/java/com/app/repository/AllVehiclesRepository.java
@@ -0,0 +1,12 @@
+package com.app.repository;
+
+import com.app.model.base.Vehicle;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AllVehiclesRepository {
+    List<Vehicle<?>> findAll();
+    List<Vehicle<?>> findByBrand(String brand);
+    Optional<Vehicle<?>> findById(Long id);
+}
