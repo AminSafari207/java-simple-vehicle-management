b7562c33fdf36d5288a1294eb54391041bea7ea4
AminSafari207 <safarin207368@gmail.com>
2025-08-25 12:50:52 +0330

    Implement TruckService class

---
+++ src/main/java/com/app/service/impl/TruckService.java
@@ -0,0 +1,28 @@
+package com.app.service.impl;
+
+import com.app.model.entity.Truck;
+import com.app.repository.TruckRepository;
+import com.app.repository.VehicleRepository;
+import com.app.repository.impl.TruckRepositoryImpl;
+import com.app.repository.impl.VehicleRepositoryImpl;
+import com.app.utils.ValidationUtils;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+public class TruckService extends VehicleServiceImpl<Long, Truck> {
+    public TruckService(EntityManagerFactory emf) {
+        super(emf);
+    }
+
+    @Override
+    protected TruckRepository repo(EntityManager em) {
+        return new TruckRepositoryImpl(em);
+    }
+
+    @Override
+    protected void validateSpecificVehicleFields(Truck v) {
+        if (v.getNumberOfAxles() < 2) throw new IllegalArgumentException("numberOfAxles must be higher than 1.");
+        if (v.getLoadCapacity() < 5D && v.getLoadCapacity() > 100D) throw new IllegalArgumentException("loadCapacity must be between 5 and 100.");
+    }
+
+}
