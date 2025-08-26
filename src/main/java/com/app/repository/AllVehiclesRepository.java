16e14c92b29ff2c789c6a5e1fabee1eb4a4c4b6b
AminSafari207 <safarin207368@gmail.com>
2025-08-26 08:40:36 +0330

    Replace AllVehiclesRepository interface generic type: Long -> ID

--- src/main/java/com/app/repository/AllVehiclesRepository.java
+++ src/main/java/com/app/repository/AllVehiclesRepository.java
@@ -5,8 +5,8 @@ import com.app.model.base.Vehicle;
 import java.util.List;
 import java.util.Optional;
 
-public interface AllVehiclesRepository {
+public interface AllVehiclesRepository<ID> {
     List<Vehicle<?>> findAll();
     List<Vehicle<?>> findByBrand(String brand);
-    Optional<Vehicle<?>> findById(Long id);
+    Optional<Vehicle<?>> findById(ID id);
 }
